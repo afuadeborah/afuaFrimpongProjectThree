65fb47e1e0ba02e5449e25af2393d53a9c85de41
Afua Frimpong <afrimpong.15@gmail.com>
2020-02-08 21:05:05 -0500

    Added favicon and fixedopening function

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ const beginButton = $('.begin');
 $(document).ready(function(){
 // We want the user to input a username which is stored and to be used on the result page
 
-
-
-
-
-// On click of the 'Let's Begin' button, we want the quiz to start and the first question to appear
     beginButton.on('click', function beginWave(){
         
         let waveName = $('input').val();
         
         if (waveName === ''){
                 alert('Please fill in a ~ wavename ~ to start the quiz.');
-            } else {
                 
-                console.log(waveName);
-            }
-        });
-        console.log('first question next');
-// NO SMOOTHIES IN ANON CALBACK
+        } else {
+            $('.letsGo').html( `<p>let's r i d e, ${waveName}.</p>`);
+            console.log(waveName);
+        }
+            $('input').val('');
+    });
+
+// DONE
+// On click of the 'Let's Begin' button, we want the quiz to start and the first question to appear
+
 
     // function beginWave(){};
     
