a0cf553efbf3b1ce792050e6257c42be36384082
Afua Frimpong <afrimpong.15@gmail.com>
2020-02-08 19:10:36 -0500

    Finished header

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Answer Array
-const aestheticSelection = [
-
-];
-
-
-
+// Global Variable Declaration
+const beginButton = $('.begin');
+const userName = $('#waveName');
 
 $(document).ready(function(){
+// We want the user to input a username which is stored and to be used on the result page
+
+
+
+
+
+// On click of the 'Let's Begin' button, we want the quiz to start and the first question to appear
+    // beginButton.on('click', beginWave(){
+
+    // });
+
+
+    // function beginWave(){};
+    
+    // function nextQuestion(){};
+    
+    // function clickSelection(){};
 
 
 
