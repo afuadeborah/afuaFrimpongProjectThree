28f6c753430a44e16b3301b8adea903c48f7dc44
Afua Frimpong <afrimpong.15@gmail.com>
2020-02-08 20:07:20 -0500

    First function up and running

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Global Variable Declaration
 const beginButton = $('.begin');
-const userName = $('#waveName');
+
 
 $(document).ready(function(){
 // We want the user to input a username which is stored and to be used on the result page
@@ -10,10 +10,19 @@ $(document).ready(function(){
 
 
 // On click of the 'Let's Begin' button, we want the quiz to start and the first question to appear
-    // beginButton.on('click', beginWave(){
-
-    // });
-
+    beginButton.on('click', function beginWave(){
+        
+        let waveName = $('input').val();
+        
+        if (waveName === ''){
+                alert('Please fill in a ~ wavename ~ to start the quiz.');
+            } else {
+                
+                console.log(waveName);
+            }
+        });
+        console.log('first question next');
+// NO SMOOTHIES IN ANON CALBACK
 
     // function beginWave(){};
     
