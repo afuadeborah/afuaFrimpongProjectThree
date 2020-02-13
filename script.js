0e6f93d43f7c009b03da642d72323e35f63ae690
Afua Frimpong <afrimpong.15@gmail.com>
2020-02-12 22:18:53 -0500

    Scoring prints to page

--- script.js
+++ script.js
@@ -2,30 +2,40 @@
 const quizQuestions = [
     {
         question: "Had to do it on 'em. Pick a pair of Vans.",
-        choices: ["./quizAssets/v-shoe.jpg", 
-        "./quizAssets/g-shoe.jpg", 
-        "./quizAssets/p-shoe.jpg"],
+        choices: [
+            "./quizAssets/v-shoe.jpg", 
+            "./quizAssets/g-shoe.jpg", 
+            "./quizAssets/p-shoe.jpg"],
         categories: ["vapor", "grunge", "pastel"]
     },
     {
         question: "Congrats, You just signed a record deal! Pick your album cover design.",
-        choices: ["1970", "1982", "1985"],
+        choices: [
+            "1970", 
+            "1982", 
+            "1985"],
         categories: ["pastel", "vapor", "grunge"]
     },
     {
         question: "You just turned 25 and are having a quarter-life crisis. Let's repaint your room. Pick a colour palette.",
-        choices: ["Seoul", "New York", "Tokyo"],
+        choices: [
+            "Seoul", 
+            "New York", 
+            "Tokyo"],
         categories: ["grunge", "pastel", "vapor"]
 
     },
     {
         question: "Pick an album.",
-        choices: ["scientist", "bus driver", "programmer"],
+        choices: [
+            "scientist", 
+            "bus driver", 
+            "programmer"],
         categories: ["vapor", "grunge", "pastel"]
 
     },
     {
-        question: "Pick a TV show.",
+        question: "You have a to do list to begin but Netflix just added more movies from the 90s . Pick a movie.",
         choices: ["1", "2", "0"],
         categories: ["pastel", "vapor", "grunge"]
 
@@ -96,7 +106,7 @@ $(document).ready(function(){
         let waveName = userName.val();
         
         if (waveName === ''){
-            alert('Please fill in a ~ wavename ~ to start the quiz.');
+            alert('Please enter a ~ wavename ~ to start the quiz.');
 
         } else {
             $('.letsGo').html(`<p>let's r i d e, ${waveName}.</p>`);
@@ -110,6 +120,7 @@ $(document).ready(function(){
 
 // Call question print 
     printQuestion();
+    
 
     
 // Proceed to the next question in the array, if we hit the last question go to results, if not display the next question
@@ -120,14 +131,13 @@ $(document).ready(function(){
     nextButton.on('click', function(){
         grabAestheticValue();
         currentQuestion++;
-        // increment the main array index by 1
-        // we can do this bc quizQuestions is in the global scope so we can grab it anywhere
+
 
         // in order to move to the next question, we have to make sure we're still within the array.length
         if (currentQuestion < quizQuestions.length){
             // keep going
             printQuestion();
-            // grabAestheticValue();
+            
         } 
         if (currentQuestion == quizQuestions.length - 1){
             // on the last question we want that click to trigger the result div
@@ -135,14 +145,13 @@ $(document).ready(function(){
             nextButton.on('click', function(){
                 
             // tally up the aesthetic array here as well
-            const resultHtml = `<h3>Your result is </h3>`;
             let vaporScore = [];
             let grungeScore = [];
             let pastelScore = [];
             // --
             for(let i = 0; i <= aestheticScore.length; i++){
                 const tallyScore = aestheticScore[i];
-                // console.log(tallyScore);
+
                 if (tallyScore === 'vapor'){
                         vaporScore.push(tallyScore);
                 } else if (tallyScore === 'grunge'){
@@ -153,21 +162,41 @@ $(document).ready(function(){
                 }
             }
             //Nested if
+                $('.question').hide();
+                $('header').hide();
+                $('.result').show().fadeIn();
+
                 if(pastelScore > grungeScore && pastelScore < vaporScore){
-                    console.log('pastel wins');
+                    let resultHtml = `
+                <h3>You are: </h3>
+                <span>Pastel</span>
+                `;
+                    $('.result').html(resultHtml);
+
                 } else if (vaporScore > grungeScore && vaporScore > pastelScore){
                     console.log('vapor wins');
+                    let resultHtml = `
+                <h3>You are: </h3>
+                <span>VaporWave</span>
+                `;
+                    $('.result').html(resultHtml )
+                    
                 } else {
                     console.log('grunge wins');
+                    let resultHtml = `
+                <h3>You are: </h3>
+                <span>Grunge</span>
+                `;
+                    $('.result').html(resultHtml);
+                    
                 }
                 // OMG IT WORKS
                 
-            
+                
 
-                $('.question').hide();
-                $('.result').show();
-                $('.result').html(resultHtml);
-                console.log(resultHtml);
+                // $('.question').hide();
+                // $('.result').show();
+
 
                 //After showing result on clock of last button, clear quiz, aesthetic array and start again
             });
